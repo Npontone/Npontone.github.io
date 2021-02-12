c90f87cc6169db0d8db69c8f53c4434ca3624d95
Npontone <Nicholaspontone@Yahoo.ca>
2021-02-12 14:04:41 -0500

    Update

--- story.js
+++ story.js
@@ -85,6 +85,7 @@ var config = {
               title: 'Chapter 3:  Highway 93',
               image: 'images/hwy.png',
               description: 'Highway 93 otherwise known as the Icefields Parkway runs from Jasper, Alberta south into the Northwestern United States. This road was built parallel to the continental divide and offers almost 230 kilometers of spectacular mountainous views (Highway 93…, Icefields Parkway). The highway is fully paved with wide shoulders and a maximum speed of 90 km/hr (Tips for…, Banff and Beyond).',
+
               location: {
                 center: { lon: -116.15278, lat: 51.00962 },
                           zoom: 10.44,
@@ -117,7 +118,7 @@ var config = {
                 id: 'Wolf-story-chapter-4',
                 title: 'Chapter 4: Wolf Mortality',
                 image: 'images/hwywolf.png',
-                description: 'Unfortunately, as the wolves often travel near the highway there are many instances where wolves have been struck by vehicles (Calgary Herald, 2014).<canvas id="myChart" width="400" height="400"></canvas>',
+                description: 'Unfortunately, as the wolves often travel near the highway there are many instances where wolves have been struck by vehicles (Calgary Herald, 2014), and railways.<canvas id="myChart" width="400" height="400"></canvas>',
                 location: {
                   center: { lon: -116.11113, lat: 51.21970 },
                             zoom: 11.47,
@@ -140,6 +141,44 @@ var config = {
                     //     opacity: 0
                     // }
                 ]
-              }
-            ]
+              },
+              {
+
+                  id: 'Wolf-story-chapter-5',
+                  title: 'Chapter 5: Adaptations',
+                  image: 'images/hwywolf.png',
+                  description: 'Thankfully, some changes have been implemented regarding the safety of wolves in surrounding parks such as Yoho and Banff National Parks. The <a href=https://calgaryherald.com/news/kootenay-national-park-gets-funding-to-reduce-wildlife-collisions-on-highway-93-s> animal underpasses </a> an Kootenay National Park are the most recent addition in these areas (added in 2013). The results in Banff National Park prove promising as there has been a reduction in wildlife vehicle collisions by 80% (Calgary Herald, 2014).<iframe width="350" height="200" src="https://www.youtube.com/embed/ND0D3bVbM7Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+                  location: {
+                    center: { lon: -115.95490, lat: 50.99710 },
+                              zoom: 13.91,
+                              pitch: 49.00,
+                              bearing: -120.36
+                  },
+                  onChapterEnter: [
+                    {
+                        layer: 'route93-2',
+                        opacity: 1
+                    },
+                    {
+                       layer: 'hwy93label',
+                       opacity: 1
+                    },
+                    {
+                        layer: 'underpasses',
+                        opacity: 1
+                    },
+                    {
+                        layer: 'underpass-label',
+                        opacity: 1
+                    },
+
+                  ],
+                  onChapterExit: [
+                      // {
+                      //     layer: 'layer-name',
+                      //     opacity: 0
+                      // }
+                  ]
+                },
+              ]
             };
